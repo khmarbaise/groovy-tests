5d271068a5d1c6ff0ac16374ea74923b72bb3387
Karl Heinz Marbaise <info@soebes.de>
2012-04-25 18:02:47 +0200

    - Addes second.groovy.

---
+++ second.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+println "This is the groovy script."
+
+1.upto(5) {
+  i -> println "i:" +i
+}
+
+(1..5).each {
+  i -> println "k:" + i
+}
+println 2**31
+println 2**64
+
+def thisIsAList = [
+  1, 2, 1, 3, 4, 5, 3
+]
+
+thisIsAList.unique().each {
+  item -> println "Unique List:" + item
+}
+
